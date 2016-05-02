8a83b209e19715b3383632c95e44cbc870508b3c
Jonas Verhoelen <jonas.verhoelen@codecentric.de>
2016-05-02 16:31:31 +0200

    added path manipulation syntax

---
+++ test/pathsTests.js
@@ -0,0 +1,17 @@
+var chai = require('chai')
+var mocha = require('mocha');
+var paths = require('./../lib/paths');
+
+var should = chai.should();
+var expect = chai.expect;
+var assert = chai.assert;
+
+describe('Paths tests', function () {
+
+    describe('Testing of path manipulations', function () {
+        it('Should be concatted properly', function () {
+            
+        });
+
+    });
+});
